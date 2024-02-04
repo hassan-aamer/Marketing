2c7e3218603fbaee0eacb0b9909b9b69b826643f
hassan-aamer <hassanaamer048@gmail.com>
2024-02-04 09:49:48 +0200

    Update

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -6,91 +6,28 @@ use App\Models\User;
 use Illuminate\Http\Request;
 
 use App\Http\Controllers\Controller;
+use App\Interfaces\Auth\AuthRepositoryInterface;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
+    private $auth;
+
+    public function __construct(AuthRepositoryInterface $auth)
+    {
+        $this->auth = $auth;
+    }
     // Registers Users
     public function createUser(Request $request)
     {
-        try {
-            //Validated
-            $validateUser = Validator::make(
-                $request->all(),
-                [
-                    'name' => 'required|string|max:255',
-                    'email' => 'required|email|unique:users,email',
-                    'password' => 'required|string|min:8|confirmed',
-                ]
-            );
-
-            if ($validateUser->fails()) {
-                return response()->json([
-                    'status' => false,
-                    'errors' => $validateUser->errors()
-                ], 401);
-            }
-
-            $user = User::create([
-                'name' => $request->name,
-                'email' => $request->email,
-                'password' => Hash::make($request->password),
-            ]);
-
-            return response()->json([
-                'status' => true,
-                'message' => 'User Created Successfully',
-                'token' => $user->createToken("API TOKEN")->plainTextToken
-            ], 200);
-        } catch (\Throwable $th) {
-            return response()->json([
-                'status' => false,
-                'message' => $th->getMessage()
-            ], 500);
-        }
+        return $this->auth->register($request);
     }
 
     // Login Users
     public function loginUser(Request $request)
     {
-        try {
-            $validateUser = Validator::make(
-                $request->all(),
-                [
-                    'email' => 'required|email',
-                    'password' => 'required'
-                ]
-            );
-
-            if ($validateUser->fails()) {
-                return response()->json([
-                    'status' => false,
-                    'errors' => $validateUser->errors()
-                ]);
-            }
-
-            $user = User::where('email', $request->email)->first();
-
-            if (!Auth::guard('api')->attempt($request->only(['email', 'password']))) {
-                return response()->json([
-                    'status' => false,
-                    'message' => 'Email & Password does not match with our record.',
-                ], 401);
-            }
-
-            return response()->json([
-                'status' => true,
-                'message' => 'User Logged In Successfully',
-                "user" => $user,
-                'token' => $user->createToken("API TOKEN")->plainTextToken
-            ], 200);
-        } catch (\Throwable $th) {
-            return response()->json([
-                'status' => false,
-                'message' => $th->getMessage()
-            ], 500);
-        }
+        return $this->auth->login($request);
     }
 }
