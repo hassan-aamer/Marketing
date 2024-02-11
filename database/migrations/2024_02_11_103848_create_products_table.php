8b16453bac672a4f73b442f1a6db613f71462fa6
hassan-aamer <hassanaamer048@gmail.com>
2024-02-11 13:00:55 +0200

    Update

--- database/migrations/2024_02_11_103848_create_products_table.php
+++ database/migrations/2024_02_11_103848_create_products_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('image');
+            $table->float('price')->unsigned()->default(0);
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
