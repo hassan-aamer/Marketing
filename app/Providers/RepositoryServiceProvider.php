9e8a9ba53ae693de2a8b065a76e8c9eb8248004c
hassan-aamer <hassanaamer048@gmail.com>
2024-02-11 13:56:10 +0200

    Update

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -8,7 +8,9 @@ use App\Repository\Products\ProductsRepository;
 use App\Interfaces\Auth\AuthRepositoryInterface;
 use App\Interfaces\Offers\OffersRepositoryInterface;
 use App\Interfaces\Products\ProductsRepositoryInterface;
+use App\Interfaces\Reviews\ReviewsRepositoryInterface;
 use App\Repository\Offers\OffersRepository;
+use App\Repository\Reviews\ReviewsRepository;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
@@ -20,6 +22,7 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(AuthRepositoryInterface::class, AuthRepository::class);
         $this->app->bind(ProductsRepositoryInterface::class, ProductsRepository::class);
         $this->app->bind(OffersRepositoryInterface::class, OffersRepository::class);
+        $this->app->bind(ReviewsRepositoryInterface::class, ReviewsRepository::class);
     }
 
     /**
---
+++ app/Repository/Reviews/ReviewsRepository.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Repository\Reviews;
+
+use App\Interfaces\Reviews\ReviewsRepositoryInterface;
+
+class ReviewsRepository implements ReviewsRepositoryInterface
+
+{
+
+}
