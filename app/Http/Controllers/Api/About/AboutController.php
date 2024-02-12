546ffd1393f95cdae73f259fa54b9e793d853787
hassan-aamer <hassanaamer048@gmail.com>
2024-02-12 22:24:29 +0200

    update

--- app/Http/Controllers/Api/About/AboutController.php
+++ app/Http/Controllers/Api/About/AboutController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\Api\About;
 
 use App\Http\Controllers\Controller;
+use App\Interfaces\About\AboutRepositoryInterface;
 use Illuminate\Http\Request;
 
 class AboutController extends Controller
 {
-    //
+    private $about;
+    public function __construct(AboutRepositoryInterface $about)
+    {
+        $this->about = $about;
+    }
+    public function update(Request $request, $id)
+    {
+        return $this->about->update($request, $id);
+    }
 }
