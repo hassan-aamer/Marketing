2f11bd147095017d4e3fb6744af710d1bfa324b5
hassan-aamer <hassanaamer048@gmail.com>
2024-02-17 21:43:12 +0200

    Changed the home page to display a message with my name and a link to my portfolio

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return ('Hassan');
+    return ('Backend development by Hassan Mphamed | https://hassan-aamer.github.io/My_Portfolio/');
 });
 
 Auth::routes();
