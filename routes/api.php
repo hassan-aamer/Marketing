b1da99d60784b0714092749ef88a2548ea2698fb
hassan-aamer <hassanaamer048@gmail.com>
2024-02-18 00:18:17 +0200

    Update

--- routes/api.php
+++ routes/api.php
@@ -41,6 +41,17 @@ Route::middleware('auth:sanctum')->group(function () {
             Route::get('/delete/{id}', 'delete');
         });
     });
+    //=============================================Offers======================================
+    Route::group(['prefix' => 'Offers'], function () {
+        Route::controller(\App\Http\Controllers\Api\offers\offerController::class)->group(function () {
+            Route::post('/create', 'create');
+            Route::post('/update/{id}', 'update');
+            Route::get('/all/Offers', 'allOffers');
+            Route::get('/Activated', 'index');
+            Route::get('/One/Offer/{id}', 'show');
+            Route::get('/delete/{id}', 'delete');
+        });
+    });
     //=============================================About======================================
     Route::group(['prefix' => 'About'], function () {
         Route::controller(\App\Http\Controllers\Api\About\AboutController::class)->group(function () {
@@ -53,17 +64,6 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::controller(\App\Http\Controllers\Api\Contact\ContactController::class)->group(function () {
             Route::post('/update/{id}', 'update');
             Route::get('/show/{id}', 'show');
-        });
-    });
-    //=============================================Offers======================================
-    Route::group(['prefix' => 'Offers'], function () {
-        Route::controller(\App\Http\Controllers\Api\offers\offerController::class)->group(function () {
-            Route::post('/create', 'create');
-            Route::post('/update/{id}', 'update');
-            Route::get('/all/Offers', 'allOffers');
-            Route::get('/Activated', 'index');
-            Route::get('/One/Offer/{id}', 'show');
-            Route::get('/delete/{id}', 'delete');
         });
     });
     //=============================================Review======================================
