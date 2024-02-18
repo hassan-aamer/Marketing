abd47ae690c015b691a258e7b70b89888b5927bf
hassan-aamer <130797396+hassan-aamer@users.noreply.github.com>
2024-02-18 11:28:53 +0200

    Create Contact Seeder

--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Contact;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,14 @@ class ContactSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Contact::create([
+            'phone'=>'01129730475',
+            'email'=>'hassanaamer048@gmail.com',
+            'url_facebook'=>'https://www.facebook.com/profile.php?id=100009255534754&locale=ar_AR',
+            'url_instagram'=>'https://www.instagram.com/hassan.m.aamer?fbclid=IwAR2x1LoY6aC3cZVmL5bu9t9tyHeHrSwu5swqsZVpswD8aCPysC8IbgD_zzE',
+            'url_twitter'=>'https://www.facebook.com/profile.php?id=100011404864121&locale=ar_AR',
+            'url_youtube'=>'https://www.linkedin.com/in/%E2%80%AAhassan-mohamed%E2%80%AC%E2%80%8F-537423264/',
+            'location'=>'Cairo Egypt',
+        ]);
     }
 }
