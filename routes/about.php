1d9db2e02c7407bff617a5434cbdab958bd705c8
hassan-aamer <hassanaamer048@gmail.com>
2024-02-20 22:17:38 +0200

    Update

--- routes/about.php
+++ routes/about.php
@@ -2,11 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::group(['prefix' => 'About'], function () {
-        Route::controller(\App\Http\Controllers\Api\About\AboutController::class)->group(function () {
-            Route::post('/update/{id}', 'update');
-            Route::get('/show/{id}', 'show');
-        });
-    });
-});
+// Route::middleware('auth:sanctum')->group(function () {
+//     Route::group(['prefix' => 'About'], function () {
+//         Route::controller(\App\Http\Controllers\Api\About\AboutController::class)->group(function () {
+//             Route::post('/update/{id}', 'update');
+//             Route::get('/show/{id}', 'show');
+//         });
+//     });
+// });
