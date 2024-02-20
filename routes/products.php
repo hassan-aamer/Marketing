1823a734b30a618136dc3ca9c888027ca46f8f64
hassan-aamer <hassanaamer048@gmail.com>
2024-02-20 23:47:45 +0200

    Update

--- routes/products.php
+++ routes/products.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
+// Route::middleware('auth:sanctum')->group(function () {
     Route::group(['prefix' => 'Products'], function () {
         Route::controller(\App\Http\Controllers\Api\Products\ProductController::class)->group(function () {
             Route::post('/create', 'create');
@@ -13,4 +13,4 @@ Route::middleware('auth:sanctum')->group(function () {
             Route::delete('/delete/{id}', 'delete');
         });
     });
-});
+// });
