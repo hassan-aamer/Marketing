8840c1fa8456b7694bfcce7a8f31f8ca5a6c1f3e
hassan-aamer <hassanaamer048@gmail.com>
2024-02-20 23:25:32 +0200

    Update

--- routes/auth.php
+++ routes/auth.php
@@ -2,14 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'Auth'], function () {
-    Route::controller(\App\Http\Controllers\Api\Auth\AuthController::class)->group(function () {
-        Route::post('/Register', 'createUser');
-        Route::post('/login', 'loginUser');
-        Route::post('/logout', 'logout');
-        Route::post('/update/{id}', 'update');
-        Route::get('/allUsers', 'allUsers');
-        Route::delete('/delete/{id}', 'delete');
-        Route::get('/show/{id}', 'show');
-    });
-});
+
