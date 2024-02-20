f5b6f7fc401215a9f0d337ec6b50975375f83077
hassan-aamer <130797396+hassan-aamer@users.noreply.github.com>
2024-02-20 14:21:48 +0200

    Update

--- app/Http/Requests/OrderRequest.php
+++ app/Http/Requests/OrderRequest.php
@@ -22,7 +22,30 @@ class OrderRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'product_id' => 'required_without:offer_id',
+            'offer_id' => 'required_without:product_id',
+            'email' => 'required|email|not_regex:/[<>]/',
+            'phone' => 'required|string|regex:/^01[0125][0-9]{8}$/|size:11|not_regex:/[<>]/',
+            'price' => 'required|numeric|min:0',
+            'payment_id' => 'required',
+            'location' => 'required|string|min:10|not_regex:/[<>]/',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'product_id.required_without' => 'Please enter either product ID or offer ID.',
+            'offer_id.required_without' => 'Please enter either product ID or offer ID.',
+            'phone.required' => 'Please enter the phone.',
+            'phone.regex' => 'The phone number is invalid. It must follow the correct format.',
+            'phone.size' => 'The phone number must be 11 characters long.',
+            'phone.not_regex' => 'Please cannot enter invalid codes.',
+            'email.required' => 'Please enter the email.',
+            'email.email' => 'The email must be a valid email address.',
+            'email.not_regex' => 'Please cannot enter invalid codes.',
+            'location.required' => 'Please enter the location.',
+            'location.not_regex' => 'Please cannot enter invalid codes.',
         ];
     }
 }
