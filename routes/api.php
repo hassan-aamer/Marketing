e751dbb26f7424e5cba4f5be7d9af0dbb3d4892f
hassan-aamer <hassanaamer048@gmail.com>
2024-02-20 23:28:13 +0200

    Update

--- routes/api.php
+++ routes/api.php
@@ -18,14 +18,3 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['prefix' => 'Auth'], function () {
-    Route::controller(\App\Http\Controllers\Api\Auth\AuthController::class)->group(function () {
-        Route::post('/Register', 'createUser');
-        Route::post('/login', 'loginUser');
-        Route::post('/logout', 'logout');
-        Route::post('/update/{id}', 'update');
-        Route::get('/allUsers', 'allUsers');
-        Route::delete('/delete/{id}', 'delete');
-        Route::get('/show/{id}', 'show');
-    });
-});
