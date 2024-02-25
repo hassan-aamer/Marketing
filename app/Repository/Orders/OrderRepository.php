e7c5919f59e02fbafbe3cf4d858bc7e540bb9ef7
hassan-aamer <hassanaamer048@gmail.com>
2024-02-25 21:57:13 +0200

    Update

--- app/Repository/Orders/OrderRepository.php
+++ app/Repository/Orders/OrderRepository.php
@@ -1,10 +1,13 @@
 <?php
 namespace App\Repository\Orders;
 
+use App\Models\offer;
+use App\Models\Order;
+use App\Models\Product;
+use Illuminate\Support\Facades\DB;
 use App\Interfaces\Orders\OrderRepositoryInterface;
 
 class OrderRepository implements OrderRepositoryInterface
-
 {
     public function index()
     {
@@ -12,7 +15,30 @@ class OrderRepository implements OrderRepositoryInterface
     }
     public function create($request)
     {
-        //
+        DB::beginTransaction();
+        try {
+            $Order = new Order();
+            $Order->product_id = $request->product_id;
+            $Order->offer_id = $request->offer_id;
+            $Order->email = $request->email;
+            $Order->phone = $request->phone;
+            $Order->price = $request->price;
+            $Order->payment_id = $request->payment_id;
+            $Order->location = $request->location;
+            $Order->save();
+
+            DB::commit();
+
+            return response([
+                "status" => true,
+                "data" => $Order,
+                "message" => "Create Order Successfully",
+            ], 200);
+
+        } catch (\Exception $e) {
+            DB::rollback();
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
     public function show($id)
     {
